7b84969bf7119e3372ed1f4f403c63b5901d7579
Alvi Muh <mohamadalvin@gmail.com>
2022-03-06 22:01:45 +0700

    Add Next and Chakra

---
+++ components/ContentCard.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { Box, Text } from "@chakra-ui/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import "swiper/css";
+
+type Props = {
+  publish_at: string;
+  data: number[];
+  content: {
+    img: string;
+  }[];
+};
+
+export default function ContentCard({ content, publish_at }: Props) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  return (
+    <Box
+      maxW="md"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      boxShadow="dark-lg"
+      mx="auto"
+      bg="gray.900"
+      border="none"
+      mb={12}
+    >
+      <Box display="flex" justifyContent="space-between">
+        <Text color="gray.400" p={4} fontWeight="medium">
+          🕓 {publish_at}
+        </Text>
+        <Text color="gray.600" p={4} fontWeight="normal">
+          {activeIndex + 1}/{content.length}
+        </Text>
+      </Box>
+      <Swiper
+        pagination={{
+          type: "progressbar",
+        }}
+        navigation={true}
+        grabCursor={true}
+        modules={[Pagination, Navigation]}
+        onSlideChange={(e) => setActiveIndex(e.activeIndex)}
+      >
+        {content.map((item, index) => (
+          <SwiperSlide key={index}>
+            <img src={item.img} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import "../style.css";
+// 2. Add your color mode config
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+  fonts: {
+    heading: "Inter, sans-serif",
+    body: "Inter, sans-serif",
+  },
+};
+
+// 3. extend the theme
+const theme = extendTheme({ config });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
